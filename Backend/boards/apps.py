1a671e98b8584ac2f1b79f0de71f712be6c6bd49
이규정 <rbwjd0101@naver.com>
2021-10-29 10:54:56 +0900

    FEAT: Django 기본 세팅

--- Backend/boards/apps.py
+++ Backend/boards/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FeatureConfig(AppConfig):
+class BoardsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'feature'
+    name = 'boards'
