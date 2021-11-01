38ce7bcd4000cbed267df7b51f200587a6957a61
jihwan0123 <qkdwlghks00@gmail.com>
2021-11-01 17:43:49 +0900

    CHORE: 데이터셋 조회 컴포넌트 수정
    데이터 조회 box에 shadow추가,  아이콘 추가 및 크기 조절
    
    
    Former-commit-id: 234db68ff546c43eda0cdda66ea8bb8033eab861

---
+++ daviz/src/pages/DetailColumn.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function DetailColumn() {
+  return (
+    <div>
+      <p>Column</p>
+    </div>
+  );
+}
---
+++ daviz/src/pages/DetailOverall.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DetailOverall() {
+  return (
+    <>
+      <p>overall</p>
+    </>
+  );
+}
+
+export default DetailOverall;
