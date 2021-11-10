98ad7735c92d9c0c8723fe8b974a58e7294c2670
jihwan0123 <qkdwlghks00@gmail.com>
2021-11-10 18:29:27 +0900

    CHORE: add toFixed
    min,max,mean,std 소수점 2자리 고정
    
    
    Former-commit-id: e837ee07f22ecb404ab2b2cfb7ea3d7dd98d5cb8

--- Frontend/src/components/DataStatistics.jsx
+++ Frontend/src/components/DataStatistics.jsx
@@ -56,10 +56,10 @@ export default function DataStatistics(props) {
           <Typography variant="body2" color="text.secondary">
             <h2>{data.name}</h2>
             <ul>
-              <li>Maximum: {data.max_val}</li>
-              <li>Mean: {data.mean}</li>
-              <li>Minimum: {data.min_val}</li>
-              <li>Std: {data.std}</li>
+              <li>Maximum: {data.max_val.toFixed(2)}</li>
+              <li>Mean: {data.mean.toFixed(2)}</li>
+              <li>Minimum: {data.min_val.toFixed(2)}</li>
+              <li>Std: {data.std.toFixed(2)}</li>
               <li>
                 Q1, Q2, Q3: {data.q1} {data.q2} {data.q3}
               </li>
